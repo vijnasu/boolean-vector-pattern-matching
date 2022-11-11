86baa0a6b1cbf25a181d6536a00bcdc65e5270ad
Hemanth Kumar G <hemanthjois@gmail.com>
2022-11-11 20:49:26 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+int main(int argc, char* argv[])
+{
+
+    std::vector<bool> haystack{0,0,0,0,1,1,1,1,1,0,1,0,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1};
+    std::vector<bool> needle{1,1,1,1};
+
+    std::vector<bool>::iterator res;
+    int dist = std::distance(needle.begin(), needle.end());
+    res = std::search(haystack.begin(), haystack.end(), needle.begin(), needle.end());
+    
+    if (res != haystack.end()) {
+        std::cout << "needle is present at index " << (res - haystack.begin()) << std::endl;
+    } else {
+        std::cout << "needle is not present in haystack" << std::endl;
+        return -1;
+    }
+    
+    while(res < haystack.end())
+    {
+      std::advance(res, dist);
+      res = std::search(res, haystack.end(), needle.begin(), needle.end());
+      
+      if(res >= haystack.end()) 
+        break;
+      
+      std::cout << "needle is present at index " << (res - haystack.begin()) << std::endl;
+    }
+    
+    return 0;
+}
+
+/*
+Output:
+
+needle is present at index 4
+needle is present at index 17
+needle is present at index 21
+
+*/
